ab45cd7193c03a08026f09d993b39a74999f1139
sabukittu <sabukittu@gmail.com>
2019-04-05 19:22:05 +0530

    ec2 'ON' or 'OFF' all instance in account

---
+++ ec2_start-stop.py
@@ -0,0 +1,28 @@
+#!/Users/kittusabu/Documents/boto3_learn/venv/bin/python
+
+import sys
+import boto3
+
+ec2client = boto3.client('ec2')
+ec2details = ec2client.describe_instances()
+
+
+a=[]
+b=[]
+c=[]
+
+for i in ec2details['Reservations']:
+    for ii in (i['Instances']):
+        a.append((ii["InstanceId"]))
+        b.append(ii["State"]["Code"])
+        c.append(ii["State"]["Name"])
+
+for i in range(len(a)):
+    if b[i] == 16:
+        ec2client.stop_instances(InstanceIds=a, DryRun=False)
+        print('The instance '+a[i]+' is stopped')
+    elif b[i] == 80:
+        ec2client.start_instances(InstanceIds=a, DryRun=False)
+        print('The instance '+a[i]+' is started')
+    else:
+        print('The instance '+a[i]+' is '+c[i])
